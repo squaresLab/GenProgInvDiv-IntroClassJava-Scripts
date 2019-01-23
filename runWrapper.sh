97a63e736a83a03ce9d918a7a5c75913a313a8ca
zhenyudg <8439020+zhenyudg@users.noreply.github.com>
2019-01-23 00:28:43 +0000

    change runWrapper preset values to those used on the ec2 server

--- runWrapper.sh
+++ runWrapper.sh
@@ -3,11 +3,11 @@
 PROJ=$1
 STNT=$2
 REVN=$3
-ICJ=/Users/zhendeveloper/Desktop/LabBox/IntroClassJava
-BGDR=/Users/zhendeveloper/Desktop/LabBox/ProgRepScripts/ICSTest
-JAVA=/Library/Java/JavaVirtualMachines/jdk1.8.0_45.jdk/Contents/Home/
+ICJ=/home/zhenyud/IntroClassJava/
+BGDR=/data
+JAVA=$JAVA_HOME
 SSED=0
-ESED=5
+ESED=9
 MODE=$4
 
 bash runGenProgForBugIntroclass.sh $PROJ $STNT $REVN $ICJ $BGDR $JAVA $SSED $ESED $MODE
