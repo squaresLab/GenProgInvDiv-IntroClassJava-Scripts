54f13e1ff3f999c01fa526d4b40b8ebbaffe3113
zhenyudg <8439020+zhenyudg@users.noreply.github.com>
2019-01-24 19:01:46 -0500

    shuts server down after batch completes

--- single_driver.py
+++ single_driver.py
@@ -13,6 +13,7 @@ def run(bug):
     print("Starting thread with output: " + outfile_name)
     call(["bash", "runWrapper.sh", proj, stdnt, rev, mode], stdout=outfile, stderr=outfile)
     call(["aws", "s3", "cp", outfile_name, "s3://gp4j-invdiv-full-results"])
+    call(["sudo", "poweroff"])
     #arefile_name = proj + "_" + stdnt + "_" + rev + "_" + "mode" + mode + ".are"
     #arefile = open(arefile_name, "w+")
     #seeds
