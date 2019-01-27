f21a311a7632bdac931cefb44f924d443bf4bd2a
zhenyudg <8439020+zhenyudg@users.noreply.github.com>
2019-01-27 21:36:33 +0000

    add runWrapper back into repo

---
+++ runWrapper.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+PROJ=$1
+STNT=$2
+REVN=$3
+ICJ=/home/zhenyud/IntroClassJava/
+BGDR=/data
+JAVA=$JAVA_HOME
+SSED=0
+ESED=9
+MODE=$4
+
+bash runGenProgForBugIntroclass.sh $PROJ $STNT $REVN $ICJ $BGDR $JAVA $SSED $ESED $MODE
